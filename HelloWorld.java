e099b28e9c75bf4ecb041bdd62d21f10bf5b8a11
admin <nancy-hdz41@hotmail.com>
2023-07-18 09:31:17 -0600

    branch prueba

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 public static void main(String[] args) {
 
-System.out.println("Hola Mundo generando conflicto :D");
+System.out.println("Hola Mundo prueba de branch");
 }
 }
